80d1bcd490efee96e658c23e0eb2e3fc3b52adf4
LBenzahia <lakhdar.benzahia@gmail.com>
2016-12-22 22:30:52 +0100

    add components

---
+++ assets/js/components/AppStem.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import StemForm from './StemForm'
+import StemmerResult from './StemmerResult'
+
+import { connect } from 'react-redux'
+import {  stemmingText  } from '../actions'
+
+
+@connect((store) => {
+   return {
+    stemResults: store.AppStems
+
+   }
+
+})
+export default class AppStem extends React.Component {
+
+    constructor(porps){
+        super(porps);
+        this.state = {value: 'مكتبة لمعالجة الكلمات العربية وتجذيعها'}
+        this.list = " "
+
+        this.handleChange = this.handleChange.bind(this);
+        this.ftechStems = this.ftechStems.bind(this);
+    }
+    handleChange(event) {
+    this.setState({value: event.target.value});
+    }
+
+    ftechStems(event){
+     this.list = this.state.value
+     this.props.dispatch(stemmingText(this.list));
+
+
+    }
+    render(){
+
+      return(
+         <div>
+            <StemForm
+             value={this.state.value}
+             onchange={this.handleChange}
+             fetchingStem={() => this.ftechStems(event)}
+
+            />
+            <StemmerResult
+            results={this.props.stemResults}
+
+            />
+         </div>
+      );
+  }
+}
